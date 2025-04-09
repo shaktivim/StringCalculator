e6436eb9d7520ba95bfa940a9a246b2ad6ddeec4
Shaktisen Chaudhary <shaktisenchaudhary@Shaktisens-MacBook-Air.local>
2025-04-09 14:35:56 +0530

    special delimiters using regrex used in calculator

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,20 +1,29 @@
-
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public String getDelimiter() {
+    public String[] getNumbersWithDelimiter(String numbers) {
         String delimiter = ",|\n"; // default delimiters having comma or newline
-        return delimiter;
+        
+        // Check for custom delimiter
+        if (numbers.startsWith("//")) {
+            int delimiterEndIndex = numbers.indexOf("\n");
+            delimiter = Pattern.quote(numbers.substring(2, delimiterEndIndex)); // Escape regex symbols
+            numbers = numbers.substring(delimiterEndIndex + 1);
+        }
+        return numbers.split(delimiter);
     }
 
     public int add(String numbers) {
+        // Will return zero if number string passed is either blank of null
         if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
         
-        String delimiter = getDelimiter();
-        String[] parts = numbers.split(delimiter);
+        // Get the parts of numbers after delimiters
+        String[] parts = getNumbersWithDelimiter(numbers);
         
+        // Initializing the sum value
         int sum = 0;
         for (String part : parts) {
             int number = Integer.parseInt(part.trim());
@@ -28,7 +37,7 @@ public class StringCalculator {
 
         System.out.println(calc.add("1,2,3"));         
         System.out.println(calc.add("1\n2,5"));         
-        // System.out.println(calc.add("//;\n1;2"));       
+        System.out.println(calc.add("//;\n1;2"));       
         // System.out.println(calc.add("//|\n2|5|7"));
     }
 }
