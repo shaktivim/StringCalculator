2318c0afdd10e233a6d057f5f740a50b68c70ec5
Shaktisen Chaudhary <shaktisenchaudhary@Shaktisens-MacBook-Air.local>
2025-04-09 14:17:31 +0530

    string sum calculation done with command and line break delimiter

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,18 +2,33 @@
 
 public class StringCalculator {
 
+    public String getDelimiter() {
+        String delimiter = ",|\n"; // default delimiters having comma or newline
+        return delimiter;
+    }
+
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
-        return 0;
+        
+        String delimiter = getDelimiter();
+        String[] parts = numbers.split(delimiter);
+        
+        int sum = 0;
+        for (String part : parts) {
+            int number = Integer.parseInt(part.trim());
+            sum += number;
+        }
+        return sum;
     }
     
     public static void main(String[] args) {
         StringCalculator calc = new StringCalculator();
 
-        System.out.println(calc.add("1\n2,3"));         
-        System.out.println(calc.add("//;\n1;2"));       
-        System.out.println(calc.add("//|\n2|5|7"));
+        System.out.println(calc.add("1,2,3"));         
+        System.out.println(calc.add("1\n2,5"));         
+        // System.out.println(calc.add("//;\n1;2"));       
+        // System.out.println(calc.add("//|\n2|5|7"));
     }
 }
